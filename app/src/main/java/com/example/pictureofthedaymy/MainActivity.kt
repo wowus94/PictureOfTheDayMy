abb9449291845726bbc1df0b8fc38e4971aecac8
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2022-07-03 13:12:24 +0300

    ДЗ 2 * Подготовил приложение под переключатель тем по клику (Есть проблемы)

--- app/src/main/java/com/example/pictureofthedaymy/MainActivity.kt
+++ app/src/main/java/com/example/pictureofthedaymy/MainActivity.kt
@@ -6,7 +6,14 @@ import androidx.core.view.WindowCompat
 import com.example.pictureofthedaymy.databinding.ActivityMainBinding
 import com.example.pictureofthedaymy.view.picture.PictureOfTheDayFragment
 
+const val ThemeDefault = 1
+const val ThemeGreen = 2
+const val ThemeRed = 3
+
 class MainActivity : AppCompatActivity() {
+
+    private val KEY_SP = "sp"
+    private val KEY_CURRENT_THEME = "current_theme"
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,12 +21,35 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
 
+        setTheme(getRealStyle(getCurrentTheme()))
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance()).commit()
+        }
+    }
+
+    fun setCurrentTheme(currentTheme: Int) {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_CURRENT_THEME, currentTheme)
+        editor.apply()
+    }
+
+    fun getCurrentTheme(): Int {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_CURRENT_THEME, -1)
+    }
+
+    private fun getRealStyle(currentTheme: Int): Int {
+        return when (currentTheme) {
+            ThemeDefault -> R.style.ThemeDefault
+            ThemeGreen -> R.style.ThemeGreen
+            ThemeRed -> R.style.ThemeRed
+            else -> 0
         }
     }
 }
--- app/src/main/java/com/example/pictureofthedaymy/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/pictureofthedaymy/view/settings/SettingsFragment.kt
@@ -1,16 +1,24 @@
 package com.example.pictureofthedaymy.view.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.pictureofthedaymy.databinding.FragmentPictureBinding
+import com.example.pictureofthedaymy.*
 import com.example.pictureofthedaymy.databinding.FragmentSettingsBinding
 import com.example.pictureofthedaymy.viewmodel.PictureOfTheDayViewModel
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var parentActivity: MainActivity
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = (context as MainActivity)
+    }
 
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
@@ -30,6 +38,33 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tabDefaultTheme.setOnClickListener(this)
+        binding.tabGreenTheme.setOnClickListener(this)
+        binding.tabRedTheme.setOnClickListener(this)
+
+        when (parentActivity.getCurrentTheme()) {
+            1 -> binding.tabLayout.check(R.id.tabDefaultTheme)
+            2 -> binding.tabLayout.check(R.id.tabGreenTheme)
+            3 -> binding.tabLayout.check(R.id.tabRedTheme)
+        }
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.tabDefaultTheme -> {
+                parentActivity.setCurrentTheme(ThemeDefault)
+                parentActivity.recreate()
+            }
+            R.id.tabGreenTheme-> {
+                parentActivity.setCurrentTheme(ThemeGreen)
+                parentActivity.recreate()
+            }
+            R.id.tabRedTheme -> {
+                parentActivity.setCurrentTheme(ThemeRed)
+                parentActivity.recreate()
+            }
+        }
     }
 
     companion object {
