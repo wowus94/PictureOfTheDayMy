a1a48c5b176be0e23efd4340cf682ac06ac2414f
Владимир Ляшук <89472439+wowus94@users.noreply.github.com>
2022-07-29 22:11:28 +0300

    ДЗ 7 * Добавил радугу в описание фотографий

--- app/src/main/java/com/example/pictureofthedaymy/view/picture/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/pictureofthedaymy/view/picture/PictureOfTheDayFragment.kt
@@ -3,13 +3,15 @@ package com.example.pictureofthedaymy.view.picture
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.text.Spannable
 import android.text.SpannableString
-import android.text.style.BackgroundColorSpan
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -30,6 +32,7 @@ class PictureOfTheDayFragment : Fragment() {
 
     private var _binding: FragmentPictureBinding? = null
     private val binding get() = _binding!!
+    lateinit var spannableString: SpannableString
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -63,6 +66,8 @@ class PictureOfTheDayFragment : Fragment() {
             }
             viewModel.sendServerRequest(currentDate.takeDate(date ?: 0))
         }
+
+
     }
 
     private fun cropImg() {
@@ -118,20 +123,55 @@ class PictureOfTheDayFragment : Fragment() {
                 binding.imageView.load(pictureOfTheDayAppState.pictureOfTheDayResponseData.url) {
                 }
 
-                val spannableString: SpannableString
                 val text = pictureOfTheDayAppState.pictureOfTheDayResponseData.explanation
-
                 spannableString = SpannableString(text)
-                val backgroundColorSpan =
-                    BackgroundColorSpan(ContextCompat.getColor(requireContext(), R.color.red))
-                spannableString.setSpan(
-                    backgroundColorSpan,
-                    0,
-                    1,
-                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                )
                 binding.textView.text = spannableString
+                rainbow(1)
+            }
+        }
+    }
 
+    fun rainbow(i: Int = 1) {
+        var currentCount = i
+        val x = object : CountDownTimer(20000, 200) {
+            override fun onTick(millisUntilFinished: Long) {
+                colorText(currentCount)
+                currentCount = if (++currentCount > 3) 1 else currentCount
+            }
+
+            override fun onFinish() {
+                rainbow(currentCount)
+            }
+        }
+        x.start()
+    }
+
+    private fun colorText(colorFirstNumber: Int) {
+        if (this@PictureOfTheDayFragment.isVisible) {
+            binding.textView.setText(spannableString, TextView.BufferType.SPANNABLE)
+            spannableString = binding.textView.text as SpannableString
+            val map = mapOf(
+                0 to ContextCompat.getColor(requireContext(), R.color.red),
+                1 to ContextCompat.getColor(requireContext(), R.color.orange),
+                2 to ContextCompat.getColor(requireContext(), R.color.white),
+                3 to ContextCompat.getColor(requireContext(), R.color.green),
+            )
+            val spans = spannableString.getSpans(
+                0, spannableString.length,
+                ForegroundColorSpan::class.java
+            )
+            for (span in spans) {
+                spannableString.removeSpan(span)
+            }
+
+            var colorNumber = colorFirstNumber
+            for (i in 0 until binding.textView.text.length) {
+                if (colorNumber == 3) colorNumber = 0 else colorNumber += 1
+                spannableString.setSpan(
+                    ForegroundColorSpan(map.getValue(colorNumber)),
+                    i, i + 1,
+                    Spannable.SPAN_EXCLUSIVE_INCLUSIVE
+                )
             }
         }
     }
